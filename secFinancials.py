3824d050168a650f31be5e5bdf78ea2ac952ec31
FleetCentere <mcgrathan@gmail.com>
2023-08-05 15:34:51 -0400

    add conditional for secfinancials to take annual based on day count

--- secFinancials.py
+++ secFinancials.py
@@ -1,4 +1,5 @@
 import requests, json, sys
+from datetime import datetime
 
 def finSearch(ticker, startYear):
     # define headers dictionary for SEC API
@@ -13,12 +14,15 @@ def finSearch(ticker, startYear):
         if tickers[row]["ticker"] == ticker.upper():
             CIK.append(str(tickers[row]["cik_str"]).zfill(10))
     
+    # CIK is a list with only one element based on when the given ticker matches the ticker of the 'tickers' json returned from SEC
     cik_str = CIK[0]
     
     company_submissions = requests.get(f"https://data.sec.gov/submissions/CIK{cik_str}.json", headers=headers).json()
     # company_concept = requests.get(f"https://data.sec.gov/api/xbrl/companyconcept/CIK{cik_str}.json", headers=headers).json()
     company_facts = requests.get(f"https://data.sec.gov/api/xbrl/companyfacts/CIK{cik_str}.json", headers=headers).json()
 
+    # determine what accounting format is used for given company in variable 'accounting'
+    # i stores what nth key is the accounting key in the 'facts' dictionary
     i = 0
     accounting = "na"
     for j, name in enumerate(list(company_facts["facts"].keys())):
@@ -26,6 +30,7 @@ def finSearch(ticker, startYear):
             i = j
             accounting = name
 
+    # generate a list storing the years for tracking
     years = []
     endYear = 2022
     if int(startYear) == int(endYear):
@@ -34,11 +39,14 @@ def finSearch(ticker, startYear):
         number = int(endYear) - int(startYear) + 1
         for i in range(number):
             years.append(int(startYear) + i)
+    
+    # list of desired financials to track
     financials_list = ["Assets", "RevenueFromContractWithCustomerExcludingAssessedTax", "CashCashEquivalentsRestrictedCashAndRestrictedCashEquivalents", 
             "CostOfRevenue", "DepreciationDepletionAndAmortization", "GeneralAndAdministrativeExpense", "GrossProfit", 
             "IncomeTaxExpenseBenefit", "Liabilities", "LongTermDebt", "NetCashProvidedByUsedInFinancingActivities", 
             "NetCashProvidedByUsedInInvestingActivities", "NetCashProvidedByUsedInOperatingActivities"]
 
+    # financials is a dictionary to store keys from financials_list with values of dictionaries for the years-values
     financials = {}
     for i in financials_list:
         financials[i] = {}
@@ -48,10 +56,19 @@ def finSearch(ticker, startYear):
             source = company_facts["facts"][accounting][key]["units"]["USD"]
             for year in years:
                 for row in source:
-                    if row["fp"] == "FY" and row["fy"] == year:
+                    dateFormat = "%Y-%m-%d"
+                    days = (datetime.strptime(row["end"], dateFormat) - datetime.strptime(row["start"], dateFormat)).days
+                    if row["fp"] == "FY" and row["fy"] == year and 360 <= days <= 370:
                         financials[key][year] = format(round(row["val"]/1000000),",")
         except KeyError:
             financials[key] = "na"
+    if __name__ == "__main__":
+        rev_source = company_facts["facts"][accounting]["RevenueFromContractWithCustomerExcludingAssessedTax"]["units"]["USD"]
+        # for row in rev_source:
+        #     print(row)
+        return financials, years, rev_source
+    
+    rev_source = company_facts["facts"][accounting]["RevenueFromContractWithCustomerExcludingAssessedTax"]["units"]["USD"]
 
     return financials, years
 
